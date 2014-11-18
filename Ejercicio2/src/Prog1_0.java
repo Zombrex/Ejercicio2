58a4640811a52eb185c6be00e4d61a91cda525bd
Angel <Angel@jose>
2014-11-18 12:19:23 +0100

    Versión 1.2.1

--- Ejercicio2/src/Prog1_0.java
+++ Ejercicio2/src/Prog1_0.java
@@ -10,6 +10,9 @@ public class Prog1_0 {
 		System.out.println("Introduce el radio de la esfera");
 		radio = leerradio.nextInt();
 		volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
+		System.out.println("El volumen de la esfera cuyo radio "+radio+" es " + volumen); 
+				System.out.println("Finalizado Version 1"); 
+
 	}
 
 }
