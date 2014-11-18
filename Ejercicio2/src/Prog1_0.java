2698e39faabe6df61ef722f5fd9d680cad8535a6
Angel <Angel@jose>
2014-11-18 12:17:43 +0100

    Programa version 1.1

--- Ejercicio2/src/Prog1_0.java
+++ Ejercicio2/src/Prog1_0.java
@@ -4,7 +4,7 @@ import java.lang.Math;
 public class Prog1_0 {
 
 	public static void main(String[] args) {
-
+		double volumen;
 		Scanner leerradio = new Scanner(System.in);
 		int radio;
 		System.out.println("Introduce el radio de la esfera");
